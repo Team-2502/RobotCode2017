474c9a863ad3fea2db82849d1380d063e0b15fec
Ritik Mishra <ritik.mishra314@gmail.com>
2017-02-11 16:29:05 -0600

    fixed the vision auton

--- src/main/java/com/team2502/robot2017/command/autonomous/AutoCommandV.java
+++ src/main/java/com/team2502/robot2017/command/autonomous/AutoCommandV.java
@@ -3,28 +3,53 @@ package com.team2502.robot2017.command.autonomous;
 import com.team2502.robot2017.command.DriveTimeCommand;
 import com.team2502.robot2017.subsystem.DriveTrainSubsystem;
 import com.team2502.robot2017.subsystem.VisionSubsystem;
+
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
 
-public class AutoCommandV extends CommandGroup
+public class AutoCommandV extends Command
 {
-    public AutoCommandV()
+	public static DriveTrainSubsystem driveTrainSubsystem;
+	public double offset;
+	public double leftSpeed;
+	public double rightSpeed;
+    
+	public AutoCommandV() 
+	{
+		driveTrainSubsystem = new DriveTrainSubsystem();
+		leftSpeed = 0.5;
+		rightSpeed = -0.5;
+	}
+    	
+    
+	@Override
+	protected void execute()
     {
-    	double offset;
-    	double leftSpeed = 1;
-    	double rightSpeed = 1;
-    	while(true){
+		while(true){
     		offset = VisionSubsystem.getOffset();
-    		if(!!(offset == 1023)){
-    			
+    		if(!(offset == 1023)){
+    			offset = offset/100;
+    			leftSpeed += offset;
+    			rightSpeed += offset;
+    			driveTrainSubsystem.runMotors(leftSpeed, rightSpeed);
     		}
-    		
-    		
-    	}
-        // addSequential(new DriveStraightCommand(1, 1));
-//        addSequential(new DriveTimeCommand(1.2));
-//        addParallel(new WaitCommand(1.0D));
+			else if(offset == 0)
+    		{
+    			leftSpeed = 0.5;
+    			rightSpeed = -0.5;
+    			driveTrainSubsystem.runMotors(leftSpeed, rightSpeed);
+    		}
+    			
+		}
     }
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}	
 }
+
+
 
