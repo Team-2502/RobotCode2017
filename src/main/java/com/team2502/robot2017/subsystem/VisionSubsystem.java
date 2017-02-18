f261507d70782155bd9a51a3402523672f1ed4e0
donovanMaas <donovan.maas@team2502.com>
2017-02-17 21:08:15 -0600

    First Attempt
    Many more to come, probably.

--- src/main/java/com/team2502/robot2017/subsystem/VisionSubsystem.java
+++ src/main/java/com/team2502/robot2017/subsystem/VisionSubsystem.java
@@ -1,15 +1,34 @@
 package com.team2502.robot2017.subsystem;
 
-import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class VisionSubsystem extends Subsystem
 {
-    private SerialPort piSerialPort;
+    private final NetworkTable table;
+
+    public VisionSubsystem()
+    {
+        table = NetworkTable.getTable("PiVision");
+    }
 
     @Override
     protected void initDefaultCommand()
     {
 
+    }
+
+    public DriveTrainSubsystem.Pair<byte[], byte[]> getContours()
+    {
+        byte[] contours0 = new byte[1000];
+        byte[] contours1 = new byte[1000];
+        contours0 = table.getRaw("contours0", contours0);
+        contours1 = table.getRaw("contours1", contours1);
+        return new DriveTrainSubsystem.Pair<byte[], byte[]>(contours0, contours1);
+    }
+
+    public void processData()
+    {
+        // TODO: Add Implementation
     }
 }
