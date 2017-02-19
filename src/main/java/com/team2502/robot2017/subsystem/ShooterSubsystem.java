cb1620be8056f1e6d71a66c0efa9430d0231dce2
isaacaj <isaac@fiaike.com>
2017-02-19 08:33:00 -0600

    Last min fixes
    Updated PID the fast way

--- src/main/java/com/team2502/robot2017/subsystem/ShooterSubsystem.java
+++ src/main/java/com/team2502/robot2017/subsystem/ShooterSubsystem.java
@@ -43,8 +43,8 @@ public class ShooterSubsystem extends Subsystem
     	
     	flywheelTalon.setProfile(0);
     	flywheelTalon.setF(0.21765900);
-    	flywheelTalon.setP(0.41312500);
-    	flywheelTalon.setI(0.001);
+    	flywheelTalon.setP(0.75312500);
+    	flywheelTalon.setI(0.0);
     	flywheelTalon.setD(0);
     }
 
