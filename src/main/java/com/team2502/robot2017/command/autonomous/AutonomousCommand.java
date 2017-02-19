87b1b7dc252a2d11b211b0f9019e35ff9689b63c
Mky135 <Mky.Flores@hotmail.com>
2017-02-18 20:04:10 -0600

    AutoCommand is better

--- src/main/java/com/team2502/robot2017/command/autonomous/AutonomousCommand.java
+++ src/main/java/com/team2502/robot2017/command/autonomous/AutonomousCommand.java
@@ -1,111 +1,111 @@
-//package com.team2502.robot2017.command.autonomous;
-//
-//import com.team2502.robot2017.DashboardData;
-//import com.team2502.robot2017.Robot;
-//import com.team2502.robot2017.RobotMap;
-//import com.team2502.robot2017.subsystem.DriveTrainSubsystem;
-//import edu.wpi.first.wpilibj.CANTalon;
-//import edu.wpi.first.wpilibj.command.Command;
-//import edu.wpi.first.wpilibj.command.Scheduler;
-//import edu.wpi.first.wpilibj.command.WaitCommand;
-//
-//@Deprecated
-//public class AutonomousCommand extends Command
-//{
-//    public final DriveTrainSubsystem driveTrainSubsystem;
-//    private final CANTalon leftTalon0;
-//    private final CANTalon leftTalon1;
-//    private final CANTalon rightTalon0;
-//    private final CANTalon rightTalon1;
-//
-//    public int m = 1000;
-//    public int L = 1000;
-//    public static double TimeStraight = 5;
-//
-//    public AutonomousCommand()
-//    {
-//        requires(Robot.DRIVE_TRAIN);
-//        driveTrainSubsystem = Robot.DRIVE_TRAIN;
-//
-//        leftTalon0 = new CANTalon(RobotMap.Motor.LEFT_TALON_0);
-//        leftTalon1 = new CANTalon(RobotMap.Motor.LEFT_TALON_1);
-//        rightTalon0 = new CANTalon(RobotMap.Motor.RIGHT_TALON_0);
-//        rightTalon1 = new CANTalon(RobotMap.Motor.RIGHT_TALON_1);
-//
-//    }
-//
-//    @Override
-//    public void initialize()
-//    {
-//
-//    }
-//
-//    @Override
-//    public void execute()
-//    {
-//        // These are test functions all will get renamed and made better/move
-//        // what we want.
-//        Scheduler.getInstance().add(new AutoCommandG());
-//        // Timer.delay(10D);
-//        Scheduler.getInstance().add(new WaitCommand(10.0D));
-//        // DriveTrainGearSwitchSubsystem.setGear(true);
-//        // Timer.delay(1D);
-//        // driveTrainSubsystem.driveA( 1, -1, 5 + 4.7625/17.98);
-//        // Timer.delay(10D);
-//        // driveTrainSubsystem.stopDriveS();
-//        // AutonomousCommand.cancelS();
-//    }
-//
-//    @Override
-//    public boolean isFinished()
-//    {
-//        return false;
-//    }
-//
-//    @Override
-//    public void end()
-//    {
-//        leftTalon0.set(0);
-//        leftTalon1.set(0);
-//        rightTalon0.set(0);
-//        rightTalon1.set(0);
-//        driveTrainSubsystem.stop();
-//    }
-//
-//    @Override
-//    public void interrupted()
-//    {
-//        driveTrainSubsystem.stop();
-//    }
-//
-//    private static AutonomousCommand instance;
-//
-//    public static void autonomousInit()
-//    {
-//        instance = DashboardData.getAutonomous();
-//    }
-//
-//    public static void startS()
-//    {
-//        // if(instance != null) { instance.start(); }
-//        Scheduler.getInstance().add(instance);
-//    }
-//
-//    public static void cancelS()
-//    {
-//        if(instance != null)
-//        {
-//            instance.cancel();
-//        }
-//    }
-//
-//    public static AutonomousCommand getInstance()
-//    {
-//        return instance;
-//    }
-//
-//    public double getTimerStraight()
-//    {
-//        return TimeStraight;
-//    }
-//}
+package com.team2502.robot2017.command.autonomous;
+
+import com.team2502.robot2017.DashboardData;
+import com.team2502.robot2017.Robot;
+import com.team2502.robot2017.RobotMap;
+import com.team2502.robot2017.subsystem.DriveTrainSubsystem;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+
+public class AutonomousCommand extends Command
+{
+    public final DriveTrainSubsystem driveTrainSubsystem;
+    private final CANTalon leftTalon0;
+    private final CANTalon leftTalon1;
+    private final CANTalon rightTalon0;
+    private final CANTalon rightTalon1;
+
+    public int m = 1000;
+    public int L = 1000;
+    public static double TimeStraight = 5;
+
+    public AutonomousCommand()
+    {
+        requires(Robot.DRIVE_TRAIN);
+        driveTrainSubsystem = Robot.DRIVE_TRAIN;
+
+        leftTalon0 = new CANTalon(RobotMap.Motor.LEFT_TALON_0);
+        leftTalon1 = new CANTalon(RobotMap.Motor.LEFT_TALON_1);
+        rightTalon0 = new CANTalon(RobotMap.Motor.RIGHT_TALON_0);
+        rightTalon1 = new CANTalon(RobotMap.Motor.RIGHT_TALON_1);
+
+    }
+
+    @Override
+    public void initialize()
+    {
+
+    }
+
+    @Override
+    public void execute()
+    {
+        // These are test functions all will get renamed and made better/move
+        // what we want.
+        Scheduler.getInstance().add(new AutoCommandG());
+        // Timer.delay(10D);
+        Scheduler.getInstance().add(new WaitCommand(10.0D));
+        // DriveTrainGearSwitchSubsystem.setGear(true);
+        // Timer.delay(1D);
+        // driveTrainSubsystem.driveA( 1, -1, 5 + 4.7625/17.98);
+        // Timer.delay(10D);
+        // driveTrainSubsystem.stopDriveS();
+        // AutonomousCommand.cancelS();
+    }
+
+    @Override
+    public boolean isFinished()
+    {
+        return false;
+    }
+
+    @Override
+    public void end()
+    {
+        leftTalon0.set(0);
+        leftTalon1.set(0);
+        rightTalon0.set(0);
+        rightTalon1.set(0);
+        driveTrainSubsystem.stop();
+    }
+
+    @Override
+    public void interrupted()
+    {
+        driveTrainSubsystem.stop();
+    }
+
+    private static AutonomousCommand instance;
+
+    public static void autonomousInit()
+    {
+        instance = DashboardData.getAutonomous();
+    }
+
+    public static void startS()
+    {
+        // if(instance != null) { instance.start(); }
+        Scheduler.getInstance().add(instance);
+    }
+
+    public static void cancelS()
+    {
+        if(instance != null)
+        {
+            instance.cancel();
+        }
+    }
+
+    public static AutonomousCommand getInstance()
+    {
+        return instance;
+    }
+
+    public double getTimerStraight()
+    {
+        return TimeStraight;
+    }
+}
