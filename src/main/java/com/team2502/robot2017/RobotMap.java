b11f907c7928a485ebfa265274a5ebba94b13f69
Mky135 <Mky.Flores@hotmail.com>
2017-02-19 20:27:46 -0600

    Added Extra Motor Build Team Wants

--- src/main/java/com/team2502/robot2017/RobotMap.java
+++ src/main/java/com/team2502/robot2017/RobotMap.java
@@ -51,6 +51,7 @@ public class RobotMap
         public static final int FEEDER_TALON_1 = 7; //banebot
         public static final int ACTIVE_INTAKE = 8;
 		public static final int CLIMBER = 9;
+		public static final int FEEDERINBOX = 10;
       
       	private Motor() {}	
     }
---
+++ src/main/java/com/team2502/robot2017/subsystem/FeederSubsystem.java
@@ -0,0 +1,40 @@
+package com.team2502.robot2017.subsystem;
+
+import com.team2502.robot2017.OI;
+import com.team2502.robot2017.RobotMap;
+import com.team2502.robot2017.command.ActiveCommand;
+import com.team2502.robot2017.command.FeederInBoxCommand;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+// TODO: Add proper implementation
+public class FeederSubsystem extends Subsystem
+{
+    private CANTalon feederInBox;
+
+    public FeederSubsystem()
+    {
+        feederInBox = new CANTalon(RobotMap.Motor.FEEDERINBOX);
+    }
+
+    @Override
+    protected void initDefaultCommand()
+    {
+        this.setDefaultCommand(new FeederInBoxCommand());
+    }
+
+    public void activeDrive()
+    {
+
+        if(OI.JOYSTICK_FUNCTION.getRawButton(3)) { feederInBox.set(1); }
+        else if(OI.JOYSTICK_FUNCTION.getRawButton(4)) { feederInBox.set(-1); }
+        else { feederInBox.set(0); }
+    }
+
+    public void stop()
+    {
+        feederInBox.set(0);
+    }
+}
+
