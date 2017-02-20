ba7c44ea59f3e4687cb0b41a4e2fa4583ebb9eeb
Mky135 <Mky.Flores@hotmail.com>
2017-02-19 21:05:33 -0600

    Fixes Gear stuff in auto

--- src/main/java/com/team2502/robot2017/command/autonomous/AutoCommandG1.java
+++ src/main/java/com/team2502/robot2017/command/autonomous/AutoCommandG1.java
@@ -2,6 +2,7 @@ package com.team2502.robot2017.command.autonomous;
 
 import com.team2502.robot2017.command.DriveTimeCommand;
 import com.team2502.robot2017.command.FlywheelCommand;
+import com.team2502.robot2017.command.NavXMoveCommand;
 import com.team2502.robot2017.command.TurnLeftCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -10,11 +11,11 @@ public class AutoCommandG1 extends CommandGroup
     public AutoCommandG1()
     {
         addSequential(new DriveTimeCommand(1.0D));
-//        addSequential(new TurnLeftCommand(1.0D));
-//        addSequential(new DriveTimeCommand(1.0D));
+        addSequential(new NavXMoveCommand(-90, 3D));
+        addSequential(new DriveTimeCommand(1.0D));
 //        addSequential(new AutoVCommand(true));
-//        addSequential(new GearCommandG(true));
-//        addSequential(new GearCommandG(false));
+        addSequential(new GearCommandG());
+        addSequential(new GearCommandG());
 //        addSequential(new AutoVCommand(false));
 //        addSequential(new FlywheelCommand(1.2D));
 
