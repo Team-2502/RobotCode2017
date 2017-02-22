52a3c5776f59ecc8accf47617ba685a1fa47108d
rohanshiva <rhnshiva@gmail.com>
2017-02-21 19:33:14 -0600

    Merge branch 'DistanceSensors' into AutonDev-ritikm
    # Conflicts:
    #	src/main/java/com/team2502/robot2017/DashboardData.java
    #	src/main/java/com/team2502/robot2017/Robot.java
    #
    src/main/java/com/team2502/robot2017/command/autonomous/AutoVCommand.jav
    a
    #
    src/main/java/com/team2502/robot2017/subsystem/DistanceSensorSubsystem.j
    ava

--- src/main/java/com/team2502/robot2017/subsystem/DistanceSensorSubsystem.java
+++ src/main/java/com/team2502/robot2017/subsystem/DistanceSensorSubsystem.java
@@ -1,9 +1,7 @@
 package com.team2502.robot2017.subsystem;
-
 import com.team2502.robot2017.RobotMap;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
-
 public class DistanceSensorSubsystem extends Subsystem
 {
 	private double distScaleFactor = 6.5231750;
@@ -11,21 +9,18 @@ public class DistanceSensorSubsystem extends Subsystem
     private AnalogInput distanceSensor;
     private double ScaleFactor = 6.532189;
   //6.532189 is a scale factor to convert voltage to distance
-
     public DistanceSensorSubsystem()
     {
         distanceSensor = new AnalogInput(RobotMap.Electrical.DISTANCE_SENSOR);
     }
-
     @Override
     protected void initDefaultCommand()
     {
         /* NO-OP */
     }
-
-   
     public double getSensorDistance()
     {
+
     if(distanceSensor.getVoltage()*distScaleFactor < 6)
     	{
     		return 20;
@@ -42,4 +37,5 @@ public class DistanceSensorSubsystem extends Subsystem
     	}
 
     }
+
 }
